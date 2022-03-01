f7e8989dd3e361e76a0c9bf9da088dbac4c7638e
Doosik Jung <entlrsla@gmail.com>
2022-03-01 23:57:02 +0900

    paint JS success

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 const canvas = document.getElementById("jsCanvas");
 const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
+
 
 const ctx = canvas.getContext("2d");
 
-canvas.width = 700;
-canvas.height = 700;
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 700;
 
-ctx.strokStyle = "#2c2c2c";
+//컨버스 사이즈 지정(html했지만 여기서도 해야됨)
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+//페이지가 시작될때 기본 배경 색지정
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
+
 let painting = false;
+let filling = false;
 
 function stopPainting(){
     painting = false;
@@ -31,23 +46,64 @@ function onMouseMove(event){
     }
 }
 
-function onMousedown(event){
-    painting = true;
-}
-
-function handleColorClick(event){
+function handleColorClick(event) {
     //console.log(event.target.style); 개발자도구에서 target에 대한 설정을 볼수있음(그래서 찾은값(backgroundColor))
     const color = event.target.style.backgroundColor;
-    console.log(typeof color);
-    ctx.strokStyle = color;
+    ctx.strokeStyle = color;
+    ctx.fillStyle = color;
+    
 }
+
+function hadelRangeChange(event) {
+    const size = event.target.value;
+    ctx.lineWidth = size;
+}
+
+function handleModeClick(){
+    if(filling === true){
+        filling = false;
+        mode.innerText = "Fill";
+    }else{
+        filling = true;
+        mode.innerText = "Paint";
+    }
+}
+
+function handleCanvasClick() {
+    if (filling) {
+      ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+    }
+  }
+
+  
+  function handleSaveClick() {
+    const image = canvas.toDataURL();
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "PaintJS";
+    link.click();
+  }
 
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("click", handleCanvasClick);
+    
 }
 
 Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
 //HTML Collection으로 받아오는걸 Array로 받아주는 구문(Array.from)
+
+if(range){
+    range.addEventListener("input", hadelRangeChange);
+}
+
+if(mode){
+    mode.addEventListener("click", handleModeClick);
+}
+
+if (saveBtn) {
+    saveBtn.addEventListener("click", handleSaveClick);
+}
